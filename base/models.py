3b477440b748b5330a15947688c3e0ab94ddc2f6
kelvinhovinho <kelvinadamba@hotmail.com>
2025-11-29 22:42:24 +0300

    crated super user and room models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host 
+    # topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # prticipants
+    updated = models.DateTimeField(auto_now=True) #changes everytime it is saved
+    created = models.DateTimeField(auto_now_add=True) #save the first time and dosenr change
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id':1, 'name':'lets learn python'},
+    {'id':2, 'name':'lets learn JAVA'},
+    {'id':3, 'name':'lets learn PHP'},
+    {'id':4, 'name':'lets learn CSS'},
+    {'id':5, 'name':'lets learn JavaScript'},
+]
+
+context = {"rooms":rooms}
+
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html',context)
 
 def room(request):
-    return render(request, 'base/room.html')
+    return render(request, 'base/room.html',context)
