26118f9bcb98ce8ea5cfd45c59bea8706e1fc0d7
kelvinhovinho <kelvinadamba@hotmail.com>
2025-12-05 22:31:17 +0300

    changed the search menu to dinamic

--- base/views.py
+++ base/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
-
+from django.db.models import Q
 from .forms import RoomForm
 
 # Create your views here.
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
 
-    rooms = Room.objects.filter(topic__name__icontains = q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     topics = Topic.objects.all
-
-
 
     context = {
         "rooms":rooms,
