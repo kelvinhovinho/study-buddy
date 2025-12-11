20d84394194292c7dfb86c18fc95bafe57da9c8e
kelvinhovinho <kelvinadamba@hotmail.com>
2025-12-11 15:05:56 +0300

    Imported login required

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from .forms import RoomForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
